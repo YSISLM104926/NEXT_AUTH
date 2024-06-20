1deb713f2e6ae5ed5c318c5830d9c5e361aa9eca
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-21 03:01:32 +0600

    added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -78,15 +78,17 @@ const LoginPage = () => {
           </form>
           <p className="text-center">Or Sign Up Using</p>
           <div className="flex justify-center mb-10 mt-2">
-            <button className="btn btn-circle ">
-              <Image
-                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
-                width={50}
-                height={50}
-                alt="google logo"
-              />
-            </button>
-            <button onClick={()=>signIn("github",{
+              <button onClick={() => signIn("google", {
+                callbackUrl: "http://localhost:3000/dashboard"
+              })} className="btn btn-circle ">
+                <Image
+                  src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                  width={50}
+                  height={50}
+                  alt="google logo"
+                />
+              </button>
+            <button onClick={() => signIn("github", {
               callbackUrl: "http://localhost:3000/dashboard"
             })} className="btn btn-circle">
               <Image
